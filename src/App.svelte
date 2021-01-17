b5a1bdea975a13699489e6b2bd722e7618a8bbdc
Vadim <korobeinikovv@mail.ru>
2021-01-17 11:15:55 +0300

    Add AddTodoItem component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script>
+	import AddTodoItem from './AddTodoItem.svelte'
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
+<AddTodoItem />
 
 <style>
 	main {
