6ba5f2724985208bd98346b27e5122216e0b3651
Vadim <korobeinikovv@mail.ru>
2021-01-18 13:03:45 +0300

    Add custom todo store

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,15 @@
 <script>
-	import { v4 as uuid } from 'uuid'
 	import AddTodoItem from './AddTodoItem.svelte'
 	import TodoItem from './TodoItem.svelte'
 	import { todoItems } from './store'
 	import { todoStats } from './todoStatsStore'
 
 	function handleAddTodoItem(event) {
-		todoItems.update(items => [...items, {
-      id: uuid(),
-			text: event.detail,
-			checked: false,
-    }])
+		todoItems.add(event.detail)
 	}
 
 	function handleItemChecked(id, checked) {
-		todoItems.update(items => items.map(item => (item.id === id
-			? ({
-				...item,
-				checked
-			})
-			: item
-		))) 
+		todoItems.setCheckedStatus(id, checked)
 	}
 </script>
 
--- src/store.js
+++ src/store.js
@@ -1,3 +1,26 @@
 import { writable } from 'svelte/store';
+import { v4 as uuid } from 'uuid'
 
-export const todoItems = writable([]);
+function createTodoStore() {
+  const { subscribe, set, update } = writable([]);
+
+  return {
+    subscribe,
+    add: text => update(items => [...items, {
+      id: uuid(),
+      text,
+      checked: false,
+    }]),
+    setCheckedStatus: (id, checked) => {
+      update(items => items.map(item => (item.id === id
+        ? ({
+          ...item,
+          checked
+        })
+        : item
+      ))) 
+    }
+  };
+}
+
+export const todoItems = createTodoStore();
