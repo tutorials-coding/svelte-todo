0dfcb617fb64b97dd9c4a6b321e7d8cac2ef0f40
Anatolii Kosorukov <java1cprog@yandex.ru>
2021-11-27 19:25:17 +0200

    Optimize phone numbers example code

--- examples/projects/video03/code/svelte-todo-07/src/components/AddTodoItem.svelte
+++ examples/projects/video03/code/svelte-todo-07/src/components/AddTodoItem.svelte
@@ -1,56 +1,41 @@
 <script>
-    import { onMount, tick } from 'svelte';
-    import { getTodos } from '../utils/getTodos';
-    import { format } from '../utils/format';
+	import { tick } from "svelte";
+	import { format } from "../utils/format";
 
-    export let title = "Enter what do you want to do.";
-    export let buttonTitle = "Add todo";
+	export let title = "Enter what do you want to do.";
 
-    let items = [];
+	let text = "";
+	async function handleTextChange(event) {
+		const { selectionStart, selectionEnd, value } = this;
+		text = format(event.target.value);
 
-    onMount(() => {
-        console.log('onMount');
-        const get = async () => {
-            items = await getTodos();
-        }
-        get();
-    });
-
-    function handleClick(){
-        items = [...items, 'item'];
-    }
-
-    let text = '';
-    async function handleTextChange(event) {
-        const { selectionStart, selectionEnd, value } = this;
-        text = format(event.target.value);
-
-        await tick();
-        this.selectionStart = selectionStart;
-        this.selectionEnd = selectionEnd;
-    }
-       
+		await tick();
+		this.selectionStart = selectionStart;
+		this.selectionEnd = selectionEnd;
+	}
 </script>
 
 <div class="main-container">
-    <label for="todo-text">{ title }</label>
-    <input class="todo-input" id="todo-text" value={text} on:input={handleTextChange} />
-    <button type="button" on:click={handleClick}>{buttonTitle}</button>
+	<label for="todo-text">{title}</label>
+	<input
+		class="todo-input"
+		id="todo-text"
+		value={text}
+		on:input={handleTextChange}
+	/>
 </div>
 
-{JSON.stringify(items, null, 2)}
-
 <style>
-    label {
-        color: cornsilk;
-        color: blueviolet;
-    }
-    .main-container{
-        background-color: lightseagreen;
-        border-radius: 5px;
-        padding: 10px;
-    }
-    .todo-input {
-        width: 100%;
-    }
+	label {
+		color: cornsilk;
+		color: blueviolet;
+	}
+	.main-container {
+		background-color: lightseagreen;
+		border-radius: 5px;
+		padding: 10px;
+	}
+	.todo-input {
+		width: 100%;
+	}
 </style>
